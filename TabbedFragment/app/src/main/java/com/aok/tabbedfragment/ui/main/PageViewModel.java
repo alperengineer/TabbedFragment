dd62f84ac9b7b0434b40bc5adada156d76bb313e
alperengineer <alperenoguzkucukcal@gmail.com>
2023-08-14 17:41:49 +0300

    TabbedFragment
    Fragment yapısını anlamak için geliştirdiğim uygulama

---
+++ TabbedFragment/app/src/main/java/com/aok/tabbedfragment/ui/main/PageViewModel.java
@@ -0,0 +1,22 @@
+package com.aok.tabbedfragment.ui.main;
+
+import static androidx.lifecycle.Transformations.map;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+
+    private MutableLiveData<String> nameInput = new MutableLiveData<>();
+
+    public void setName(String name) {
+        nameInput.setValue(name);
+    }
+
+    public LiveData<String> getName(){
+        return nameInput;
+    }
+
+}
